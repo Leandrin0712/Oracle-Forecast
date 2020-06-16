9c9813f14428379cf4e9999565aedfc87d644f39
Leandrin0712 <65507576+Leandrin0712@users.noreply.github.com>
2020-06-16 15:53:58 -0300

    Add files via upload

---
+++ teste.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Oracle Forecast</title>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+       <h1>Oracle Forecast</h1>
+
+       <h2>
+             <?php
+             $cidade = $_POST["cidade"];
+                echo $cidade;
+             ?>
+        </h2>
+        <h3>
+
+
+            <?php
+                $cidade = $_POST["cidade"];
+        		shell_exec("python3 teste.py {$cidade}");
+        		$condicao = file_get_contents("condicao.txt");
+        		$temperatura = file_get_contents("temperatura.txt");
+                
+                echo "<br>";
+        		echo $condicao;
+        		echo "<br>";
+        		echo $temperatura;
+        	?>
+
+
+            	</h3>
+</body>
+</html>
