52435637e0b3dd5c861342400e35806fffb43dd1
Leandrin0712 <65507576+Leandrin0712@users.noreply.github.com>
2020-06-16 15:52:59 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,13 +1,20 @@
 import requests
 import json
+import sys
 
-cidade = input("Escreva sua cidade: ")
+cidade = sys.argv[1]
+print(cidade)
 
 requisicao = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+cidade+'&appid=8947a1be67924f45dc1f5b77fd8ef54f')
 
 tempo = json.loads(requisicao.text)
-print('condição do tempo:', tempo['weather'][0]['main'])
-print('temperatura', float(tempo['main']['temp']) - 273.15)
+
+sys.stdout = open("condicao.txt", "w")
+print('Condição do tempo:', tempo['weather'][0]['main'])
 
 
+sys.stdout.close()
 
+sys.stdout = open("temperatura.txt", "w")
+print('Temperatura:', float(tempo['main']['temp']) - 273.15)
+sys.stdout.close()
