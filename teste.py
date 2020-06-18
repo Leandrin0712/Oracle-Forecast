99b8bda943c259599e0f38e57e917ed12af0221f
Leandrin0712 <65507576+Leandrin0712@users.noreply.github.com>
2020-06-17 21:09:27 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,28 +2,28 @@ import requests
 import json
 import sys
 
+tamanho = len(sys.argv) - 1
 
-cidade = sys.argv[1]
-cidadeFormatada = cidade.replace('-', ' ')
-
-condicao = tempo['weather'][0]['main']
-
-temperatura = float(tempo['main']['temp']) - 273.15
-temperaturaFormatada = round(temperatura, 2)
+if tamanho == 1:
+	cidade = sys.argv[1]
+elif tamanho == 2:
+	cidade = '{} {}'.format(sys.argv[1], sys.argv[2])
+else:
+	cidade = '{} {} {}'.format(sys.argv[1], sys.argv[2], sys.argv[3])
 
 # Faz a requisição da previsão 
 
-requisicao = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+cidade+'&appid=8947a1be67924f45dc1f5b77fd8ef54f')
+requisicao = requests.get('http://api.openweathermap.org/data/2.5/weather?q=' + cidade + '&appid=8947a1be67924f45dc1f5b77fd8ef54f')
 tempo = json.loads(requisicao.text)
 
 # Gera os arquivos para mostrar no site
 
 sys.stdout = open("condicao.txt", "w")
-
-print('Condição do tempo:', condicao)
+print('Condição do tempo:', tempo['weather'][0]['main'])
 sys.stdout.close()
 
 sys.stdout = open("temperatura.txt", "w")
-
-print('Temperatura:', temperaturaFormatada)
+temperatura = float(tempo['main']['temp']) - 273.15
+temperaturaArredondada = round(temperatura, 2)
+print('Temperatura:', temperaturaArredondada)
 sys.stdout.close()
